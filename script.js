50f9b8797d845cb4e62109db10b2cf972b7a8c81
kellysamara2025 <kelly.favretto@escola.pr.gov.br>
2025-10-03 11:58:07 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.queryelectora11('.btn-proximo');
+
+avanca.foreach(buton => {
+    button.addeventlistener('click', function(){
+        const atua1 = document.querys1ctor('.ativo');
+    })
+})
