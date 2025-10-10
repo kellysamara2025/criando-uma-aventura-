2a80bd970fa19fcc0b4973e59f7afadd188eaa23
kellysamara2025 <kelly.favretto@escola.pr.gov.br>
2025-10-10 11:47:21 -0300

    .

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const avanca = document.queryelectora11('.btn-proximo');
 avanca.foreach(buton => {
     button.addeventlistener('click', function(){
         const atua1 = document.querys1ctor('.ativo');
+        const proximopasso = 'passo-' + this.getattribute('data-proximo');
+
+        atual.classlist.rmove('ativo');
+        document.getElementById(proximopsso).classlist.add('ativo');
     })
 })
